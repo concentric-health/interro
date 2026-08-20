c5f973da595c8fcdfe8a6a954f56813949d6f0d6
Martyn Loughran <martyn@mloughran.com>
2026-08-20 09:22:47 +0100

    Refactor to remove placeholder numbering from QueryValue internals
    Separated from the last commit for clarity; `QueryValue` now constructs `QueryExpression` parts directly rather than parsing a numbered expression.
    
    By removing the now unused `QueryExpression.new(lhs, comparator, rhs, values)` constructor, all SQL expression parsing always goes through `QueryExpression.parse` (making it easy to spot), and is now only used for raw fragments from user code.

--- src/query_value.cr
+++ src/query_value.cr
@@ -4,41 +4,40 @@ require "./query_expression"
 module Interro
   struct QueryValue
     getter value : String
-    getter index : Int32
 
-    def initialize(@value, @index)
+    def initialize(@value)
     end
 
     def ==(other : Value)
-      QueryExpression.new(value, "=", "$#{index}", [Any.new(other)])
+      QueryExpression.new("#{value} = ", Any.new(other))
     end
 
     def ==(other : Nil)
-      QueryExpression.new(value, "IS", "NULL", [] of Any)
+      QueryExpression.new("#{value} IS NULL")
     end
 
     def <=(other : Value)
-      QueryExpression.new(value, "<=", "$#{index}", [Any.new(other)])
+      QueryExpression.new("#{value} <= ", Any.new(other))
     end
 
     def >=(other : Value)
-      QueryExpression.new(value, ">=", "$#{index}", [Any.new(other)])
+      QueryExpression.new("#{value} >= ", Any.new(other))
     end
 
     def <(other : Value)
-      QueryExpression.new(value, "<", "$#{index}", [Any.new(other)])
+      QueryExpression.new("#{value} < ", Any.new(other))
     end
 
     def >(other : Value)
-      QueryExpression.new(value, ">", "$#{index}", [Any.new(other)])
+      QueryExpression.new("#{value} > ", Any.new(other))
     end
 
     def !=(other : Value)
-      QueryExpression.new(value, "!=", "$#{index}", [Any.new(other)])
+      QueryExpression.new("#{value} != ", Any.new(other))
     end
 
     def !=(other : Nil)
-      QueryExpression.new(value, "IS NOT", "NULL", [] of Any)
+      QueryExpression.new("#{value} IS NOT NULL")
     end
 
     def in?(array : Enumerable(Value))
@@ -46,7 +45,7 @@ module Interro
     end
 
     def in?(array : Enumerable(Any))
-      QueryExpression.new(value, "=", "ANY($#{index})", [Any.new(array)])
+      QueryExpression.new("#{value} = ANY(", Any.new(array), ")")
     end
 
     def not_in?(array : Enumerable(Value))
@@ -54,13 +53,13 @@ module Interro
     end
 
     def not_in?(array : Enumerable(Any))
-      QueryExpression.new(value, "!=", "ALL($#{index})", [Any.new(array)])
+      QueryExpression.new("#{value} != ALL(", Any.new(array), ")")
     end
 
     {% for operator in %w[& | ^] %}
       # Bitwise operator
       def {{operator.id}}(other : Value)
-        QueryExpression.new(value, {{operator}}, "$#{index}", [Any.new(other)])
+        QueryExpression.new("#{value} {{operator.id}} ", Any.new(other))
       end
     {% end %}
   end
